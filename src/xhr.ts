d6e5d3715416e17c658d362e9e1a29d95fc57d7e
lambert1129 <lambert1129@foxmail.com>
2020-01-24 13:59:16 +0800

    feat(xhr): 添加xhr函数

---
+++ src/xhr.ts
@@ -0,0 +1,12 @@
+import {AxiosRequestConfg} from './types'
+
+export default function xhr (config:AxiosRequestConfg) {
+  // TODO
+  const {data=null,url,method='get'} = config
+
+  const request = new XMLHttpRequest()
+
+  request.open(method.toUpperCase(),url,true)
+
+  request.send(data)
+}
