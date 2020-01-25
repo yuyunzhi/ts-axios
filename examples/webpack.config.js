cda84b6408d77ad88dda3564c940ee736e057fd6
lambert1129 <lambert1129@foxmail.com>
2020-01-25 14:50:33 +0800

    docs: 添加webpack打包函数说明

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -4,14 +4,21 @@ const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
-
+  /**
+   * entries最后的结构如下：
+   * entries { base:
+   [ 'webpack-hot-middleware/client',
+     '/Users/yuyunzhi/Desktop/yuyunzhi/ts-axios/examples/base/app.ts' ],
+  simple:
+   [ 'webpack-hot-middleware/client',
+     '/Users/yuyunzhi/Desktop/yuyunzhi/ts-axios/examples/simple/app.ts' ] }
+   */
   entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
     const fullDir = path.join(__dirname, dir)
     const entry = path.join(fullDir, 'app.ts')
     if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
       entries[dir] = ['webpack-hot-middleware/client', entry]
     }
-
     return entries
   }, {}),
 
