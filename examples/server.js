ae8c289faa44271aa2ed6ba24d164c91fd9b8247
lambert1129 <lambert1129@foxmail.com>
2020-01-25 12:21:38 +0800

    style: 移动router位置

--- examples/server.js
+++ examples/server.js
@@ -23,11 +23,6 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 8080
-module.exports = app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
-})
-
 const router = express.Router()
 
 router.get('/simple/get', function(req, res) {
@@ -37,3 +32,9 @@ router.get('/simple/get', function(req, res) {
 })
 
 app.use(router)
+
+const port = process.env.PORT || 8080
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
+})
+
