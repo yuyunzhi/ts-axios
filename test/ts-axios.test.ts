8b5fcd63fb92476149b4cdd6909fd99a714d5874
lambert1129 <lambert1129@foxmail.com>
2020-01-26 20:00:40 +0800

    fix: 单元测试

--- test/ts-axios.test.ts
+++ test/ts-axios.test.ts
@@ -1,14 +1,10 @@
-import DummyClass from "../src/ts-axios"
+import DummyClass from '../src/axios'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
