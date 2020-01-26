95a6ef7b69447e0d3bf4f0ea558814ebb0b52ab5
lambert1129 <lambert1129@foxmail.com>
2020-01-26 12:21:15 +0800

    feat(添加header解析的用例):

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -87,23 +87,46 @@ import axios from '../../src/index'
 //   }
 // })
 
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   headers: {
+//     'content-type': 'application/json;charset=utf-8'
+//   },
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+//
+// const paramsString = 'q=URLUtils.searchParams&topic=api'
+// const searchParams = new URLSearchParams(paramsString)
+//
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: searchParams
+// })
+
 axios({
   method: 'post',
   url: '/base/post',
-  headers: {
-    'content-type': 'application/json;charset=utf-8'
-  },
   data: {
     a: 1,
     b: 2
   }
+}).then((res) => {
+  console.log('第一个',res)
 })
-
-const paramsString = 'q=URLUtils.searchParams&topic=api'
-const searchParams = new URLSearchParams(paramsString)
 
 axios({
   method: 'post',
   url: '/base/post',
-  data: searchParams
+  responseType: 'json',
+  data: {
+    a: 3,
+    b: 4
+  }
+}).then((res) => {
+  console.log('第二个',res)
 })
