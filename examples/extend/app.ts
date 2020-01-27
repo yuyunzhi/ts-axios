c68ef01fa289f3e365018268e77df59a6aad648c
lambert1129 <lambert1129@foxmail.com>
2020-01-27 12:01:25 +0800

    style: 添加测试用例

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -6,26 +6,45 @@ axios({
   data: {
     msg: 'hi'
   }
+}).then(res => {
+  console.log('hi', res)
 })
 
-axios.request({
-  url: '/extend/post',
+axios('/extend/post', {
   method: 'post',
   data: {
     msg: 'hello'
   }
+}).then(res => {
+  console.log('hello', res)
 })
 
-axios.get('/extend/get')
-
-axios.options('/extend/options')
-
-axios.delete('/extend/delete')
-
-axios.head('/extend/head')
-
-axios.post('/extend/post', { msg: 'post' })
-
-axios.put('/extend/put', { msg: 'put' })
-
-axios.patch('/extend/patch', { msg: 'patch' })
+// axios({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hi'
+//   }
+// })
+//
+// axios.request({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
+//
+// axios.get('/extend/get')
+//
+// axios.options('/extend/options')
+//
+// axios.delete('/extend/delete')
+//
+// axios.head('/extend/head')
+//
+// axios.post('/extend/post', { msg: 'post' })
+//
+// axios.put('/extend/put', { msg: 'put' })
+//
+// axios.patch('/extend/patch', { msg: 'patch' })
