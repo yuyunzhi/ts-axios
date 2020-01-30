b01d7f441536d0325eda6c27381c45ae69bc8723
lambert1129 <lambert1129@foxmail.com>
2020-01-30 10:47:44 +0800

    style: 用例 create添加

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -19,8 +19,27 @@ axios.defaults.headers.common['test2'] = 123
 // }).then((res) => {
 //   console.log(res.data)
 // })
+//
+// axios({
+//   transformRequest: [(function(data) {
+//     return qs.stringify(data)
+//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
+//     if (typeof data === 'object') {
+//       data.b = 2
+//     }
+//     return data
+//   }],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then((res) => {
+//   console.log(res.data)
+// })
 
-axios({
+const instance = axios.create({
   transformRequest: [(function(data) {
     return qs.stringify(data)
   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
@@ -29,12 +48,15 @@ axios({
       data.b = 2
     }
     return data
-  }],
+  }]
+})
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
     a: 1
   }
 }).then((res) => {
-  console.log(res.data)
+  console.log('lalalla',res.data)
 })
