51116d1937b8573f76b5b90737915cb9c54232ff
lambert1129 <lambert1129@foxmail.com>
2020-01-30 10:46:35 +0800

    feat: 实现静态类接口axios.create()创建一个新的axios

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosInstance, AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extendAixos } from './helpers/util'
 import defaults from './default'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const _this = new Axios(config)
   console.log('_this', _this)
   const instance = Axios.prototype.request.bind(_this)
   extendAixos(instance, _this)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function create(config: AxiosRequestConfig) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
