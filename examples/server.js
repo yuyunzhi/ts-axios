77ab4f30dde284e0e1b267a8b565fc5af8d3db8a
lambert1129 <lambert1129@foxmail.com>
2020-01-31 12:36:31 +0800

    feat: 添加server2 && 添加用例返回

--- examples/server.js
+++ examples/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
 
+require('./server2')
 const app = express()
 const compiler = webpack(WebpackConfig)
 
@@ -22,6 +24,7 @@ app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 const router = express.Router()
 
@@ -130,6 +133,11 @@ router.post('/config/post', function(req, res) {
     msg: `post`
   })
 })
+
+router.get('/more/get', function(req, res) {
+  res.json(req.cookies)
+})
+
 app.use(router)
 
 const port = process.env.PORT || 8080
