e29468dd373f387c930078e84059738a48ec478c
lambert1129 <lambert1129@foxmail.com>
2020-02-26 18:51:09 +0800

    添加util文件的单元测试

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,15 +1,9 @@
-import instantiate = WebAssembly.instantiate
-
 export function isDate(value: any): boolean {
   return Object.prototype.toString.call(value) === `[object Date]`
 }
 
 export function isCommonObject(value: any): any {
   return Object.prototype.toString.call(value) === `[object Object]`
-}
-
-export function isObject(value: any): boolean {
-  return value !== null && typeof value === 'object'
 }
 
 export function extendAixos<T, U>(to: T, from: U): T & U {
